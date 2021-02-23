035f0be13483251e1f3ca5e6bade14b318b290fa
yesha-mobio <yeshas.mobio@gmail.com>
2021-02-23 15:57:55 +0530

    Config file change

--- config/config.js
+++ config/config.js
@@ -13,14 +13,14 @@ const {
 } = process.env;
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
+  apiKey: "AIzaSyDWcZSbyp_kYJSNxLRVVemkx_5V9JlQDHA",
+  authDomain: "logistics-ondemand-app.firebaseapp.com",
+  databaseURL: "https://logistics-ondemand-app.firebaseio.com",
+  projectId: "logistics-ondemand-app",
+  storageBucket: "logistics-ondemand-app.appspot.com",
+  messagingSenderId: "335686843365",
+  appId: "1:335686843365:web:d4bb2a7c82af72fcdf8d0c",
+  measurementId: "G-7WPCXTQJ8N",
 };
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
