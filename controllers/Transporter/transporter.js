fddf7016da771e973933166469e9f6ce1828cdc7
pasmina <pasminar.mobio@gmail.com>
2021-10-20 18:30:15 +0530

    - Changes for transporter file upload

--- controllers/Transporter/transporter.js
+++ controllers/Transporter/transporter.js
@@ -172,7 +172,6 @@ exports.newTransporter = async (req, res) => {
 
       let status = await isBoolean(data.status);
       // let registered = await isBoolean(data.registered);
-
       const profilePublicUrl = await imagePublicUrl(files.profile);
       const addressProofPublicUrl = await imagePublicUrl(files.AddressProof);
       const identityProofPublicUrl = await imagePublicUrl(files.IdentityProof);
@@ -225,37 +224,37 @@ exports.newTransporter = async (req, res) => {
         status: status,
         // user_uid: newTransporter.user.uid,
       };
-      console.log(req.files,"req files*****");
-      let iconss = [];
-      for (var i = 0; i < req.files.length; i++) {
-        console.log(req.files,"req files1****");
-        const iconId = uuidv4();
-        let base64 = req.files[i].buffer.toString("base64");
-        let mimetype = req.files[i].mimetype;
-        const nameArr = req.files[i].originalname.split(".")
-        const fileLocation = `vehicle-details/${iconId}.${(nameArr.length !== 0) ? nameArr[nameArr.length - 1] : ""}`
-        const file = bucket.file(fileLocation)
-        await file.save(req.files[i].buffer, { contentType: mimetype })
-        await file.setMetadata({
-          firebaseStorageDownloadTokens: iconId
-        })
+      // console.log(req.files,"req files*****");
+      // let iconss = [];
+      // for (var i = 0; i < req.files.length; i++) {
+      //   console.log(req.files,"req files1****");
+      //   const iconId = uuidv4();
+      //   let base64 = req.files[i].buffer.toString("base64");
+      //   let mimetype = req.files[i].mimetype;
+      //   const nameArr = req.files[i].originalname.split(".")
+      //   const fileLocation = `vehicle-details/${iconId}.${(nameArr.length !== 0) ? nameArr[nameArr.length - 1] : ""}`
+      //   const file = bucket.file(fileLocation)
+      //   await file.save(req.files[i].buffer, { contentType: mimetype })
+      //   await file.setMetadata({
+      //     firebaseStorageDownloadTokens: iconId
+      //   })
         
-        const icon = {
-          id: iconId,
-          base64: base64,
-          dataUrl: `data:${mimetype};${base64}`,
-          url: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(fileLocation)}?alt=media&token=${iconId}`,
-          type: mimetype,
-        };
-        iconss.push(icon);
-      }
+      //   const icon = {
+      //     id: iconId,
+      //     base64: base64,
+      //     dataUrl: `data:${mimetype};${base64}`,
+      //     url: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(fileLocation)}?alt=media&token=${iconId}`,
+      //     type: mimetype,
+      //   };
+      //   iconss.push(icon);
+      // }
 
       const vehicleData = {
         vehicle_type: data.vehicleTypeName,
         vehicle_number: data.VehicleNumber,
         comment: data.Comments,
         chassis_number: data.ChassisNumber,
-        vehicle_photos: iconss,
+        vehicle_photos: typeof icons === "string" ? [icons] : icons,
         created_at: new Date(),
         is_assign: false,
         is_deleted: false,
