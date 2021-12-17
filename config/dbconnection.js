1b8be7fd99e76e03ac480200acfa8da087b1a2e8
pasmina <pasminar.mobio@gmail.com>
2021-12-17 14:28:23 +0530

    - Fixing Issues for setup

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-// DB Connection Start
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(process.env.MONGO_DATABASE, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("CONNECTION SUCCESSFUL"))
-  .catch((err) => console.log(err));
-// DB Connection End
+if (process.env.MONGO_DATABASE) {
+  // DB Connection Start
+  mongoose.Promise = global.Promise;
+  mongoose
+    .connect(process.env.MONGO_DATABASE, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useFindAndModify: false,
+      // useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("CONNECTION SUCCESSFUL"))
+    .catch((err) => console.log(err));
+  // DB Connection End
+}
