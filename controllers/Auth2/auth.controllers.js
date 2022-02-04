aeb7b4150e847e8263de7924332c82b394c3c89e
Kiran <kiranb.mobio@gmail.com>
2022-02-04 20:27:58 +0530

    try to fix

--- controllers/Auth2/auth.controllers.js
+++ controllers/Auth2/auth.controllers.js
@@ -2,11 +2,10 @@ const userService = require("./auth.services");
 const { db, firebase } = require("../../config/admin");
 const { validateData, validateData2, validateData3 } = require("./validations");
 
-
 // Sign Up
 exports.signup = async (req, res, next) => {
   try {
-    console.log(req.body,"All data")
+    console.log(req.body, "All data");
     //let user_uid = req.body.userUid;
     const login_data = req.body;
     const user = {
@@ -32,10 +31,12 @@ exports.signup = async (req, res, next) => {
     });
 
     if (foundData) {
-      errors.push({ msg: "User already exists!" });
-      return res.render("Payment/login", {
-        errors,
-      });
+      console.log("1234");
+      next();
+      //   errors.push({ msg: "User already exists!" });
+      //   return res.render("Payment/login", {
+      //     errors,
+      //   });
     }
     // let fullname = req.body.name;
     // let firstName = fullname.split(" ").slice(0, -1).join(" ");
@@ -53,9 +54,8 @@ exports.signup = async (req, res, next) => {
     // };
     await userService.creatNewUser(user);
     //await userService.creatNewTransporter(transporter,user_uid);
-  //  return res.render("Payment/otp", login_data);
-  next()
-   
+    //  return res.render("Payment/otp", login_data);
+    next();
   } catch (error) {
     const errors = [];
     console.log(error);
@@ -92,44 +92,43 @@ exports.sendOtp = async (req, res, next) => {
 exports.verifyOtp = async (req, res, next) => {
   const name = req.body.name;
   const phone_number = req.body.phone_number;
-  
+
   try {
-    console.log(req.body,"verification data")
+    console.log(req.body, "verification data");
     let id, full_name, phone;
     let user_id = req.body.userUid;
     const user_data = req.body;
     //const { errors } = validateData2();
-    
-      const data = await db
-          .collection("anonymous")
-          .where("phone_number", "==", req.body.phone_number);
-        await data.get().then(function (querySnapshot) {
-          querySnapshot.forEach(function (doc) {
-            console.log(doc.id, " => ", doc.data());
-            id = doc.id;
-            (full_name = doc.data().name), (phone = doc.data().phone_number);
-          });
-        });
-        const users = {
-          is_verified: "verified",
-        };
-        const my_data = await db.collection("anonymous").doc(id);
-        await my_data.update(users);
-        let fullname = full_name;
-        let firstName = fullname.split(" ").slice(0, -1).join(" ");
-        let lastName = fullname.split(" ").slice(-1).join(" ");
-        const user = {
-          first_name: firstName,
-          last_name: lastName,
-          phone_number: phone_number,
-          is_registered: false,
-          user_type: "transporter",
-          is_verified: "verified",
-        };
-        await userService.creatNewTransporter(user,user_id);
-      //  return res.render("Payment/transporterdetails", user_data);
-        next();
-  
+
+    const data = await db
+      .collection("anonymous")
+      .where("phone_number", "==", req.body.phone_number);
+    await data.get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        console.log(doc.id, " => ", doc.data());
+        id = doc.id;
+        (full_name = doc.data().name), (phone = doc.data().phone_number);
+      });
+    });
+    const users = {
+      is_verified: "verified",
+    };
+    const my_data = await db.collection("anonymous").doc(id);
+    await my_data.update(users);
+    let fullname = full_name;
+    let firstName = fullname.split(" ").slice(0, -1).join(" ");
+    let lastName = fullname.split(" ").slice(-1).join(" ");
+    const user = {
+      first_name: firstName,
+      last_name: lastName,
+      phone_number: phone_number,
+      is_registered: false,
+      user_type: "transporter",
+      is_verified: "verified",
+    };
+    await userService.creatNewTransporter(user, user_id);
+    //  return res.render("Payment/transporterdetails", user_data);
+    next();
   } catch (error) {
     console.log(error);
     const errors = [];
@@ -143,17 +142,16 @@ exports.verifyOtp = async (req, res, next) => {
 };
 
 exports.updateTransporter = async (req, res, next) => {
-  console.log(req.body,"mydata")
-  let user_data= req.body;
+  console.log(req.body, "mydata");
+  let user_data = req.body;
   let name = req.body.name;
   let phone_number = req.body.phone_number;
   let userUid = req.body.userUid;
   try {
-    
     //let id;
     const user = {
       email: req.body.email,
-      gst_number: req.body.gstNo
+      gst_number: req.body.gstNo,
     };
     // const data = await db
     //   .collection("users")
@@ -167,11 +165,11 @@ exports.updateTransporter = async (req, res, next) => {
     const my_data = await db.collection("users").doc(userUid);
     await my_data.update(user);
     return res.render("Payment/appUrl", {
-     user_data
+      user_data,
     });
   } catch (error) {
     const errors = [];
-    console.log(error,"errorrs")
+    console.log(error, "errorrs");
     if (error.code == "auth/email-already-in-use") {
       errors.push({ msg: "Email already exists!" });
     }
@@ -179,7 +177,7 @@ exports.updateTransporter = async (req, res, next) => {
       errors,
       phone_number,
       name,
-      userUid
+      userUid,
     });
     // const errors = [];
     // errors.push({ msg: error.message });
